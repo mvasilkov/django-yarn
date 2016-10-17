e432a981c61e0f942a9c42746c596d1ce005fe02
Mark Vasilkov <mvasilkov@gmail.com>
2016-10-17 15:58:19 +0300

    Packaging

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+if __name__ == '__main__':
+    setup(
+        name='django-yarn',
+
+        version='0.0.1',
+
+        description='Use Facebook\'s Yarn package manager with Django.',
+        long_description='Use Facebook\'s Yarn package manager with Django.',
+
+        url='https://github.com/mvasilkov/django-yarn',
+
+        author='Mark Vasilkov',
+        author_email='mvasilkov@gmail.com',
+
+        license='MIT',
+
+        classifiers=[
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 3',
+        ],
+
+        keywords='django yarn',
+
+        packages=find_packages(),
+    )
